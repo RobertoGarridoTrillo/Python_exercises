b0bae67edbfb941e829aa366c8854528adf0ce98
Roberto Garrido Trillo <robegt72@gmail.com>
2021-07-13 05:42:48 +0200

    Add print_bye() function

--- 01HelloPython/main.py
+++ 01HelloPython/main.py
@@ -2,5 +2,10 @@ def print_hi(name):
     print("Hello, {:>30}".format(name))
 
 
+def print_bye(name):
+    print("Bye,{:_>33}".format(name))
+
+
 if __name__ == '__main__':
     print_hi('World!')
+    print_bye('Galaxy!')
