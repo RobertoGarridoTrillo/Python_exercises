f7a5f1982dea92ef5bdafa783040b40a6959391a
Roberto Garrido Trillo <robegt72@gmail.com>
2021-07-13 06:02:31 +0200

    Creating a new branch (delevoper). Creating a new function (my_funtion)

--- 01HelloPython/main.py
+++ 01HelloPython/main.py
@@ -6,6 +6,11 @@ def print_bye(name):
     print("Bye,{:_>33}".format(name))
 
 
+def my_function(a, b):
+    return a + b
+
+
 if __name__ == '__main__':
     print_hi('World!')
     print_bye('Galaxy!')
+    print(my_function(1, 2))
